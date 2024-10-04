1bc4388f010e4e999d012cf3e15a569759b7880c
Alok Nath Pandey <125994027+aloknathpandey@users.noreply.github.com>
2024-10-04 23:51:35 +0530

    get api demo

--- src/NewsItem.js
+++ src/NewsItem.js
@@ -11,7 +11,7 @@ export class NewsItem extends Component {
                         <div className="card-body">
                             <h5 className="card-title">{title}</h5>
                             <p className="card-text">{description}...</p>
-                            <a href={url} className="btn btn-sm btn-primary">More</a>
+                            <a href={url} rel="noreferrer" target="_blank" className="btn btn-sm btn-primary">More</a>
                         </div>
                 </div>
             </div>
