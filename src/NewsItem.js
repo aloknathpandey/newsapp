d2f2420d54102102d360807e3d3063ec18ce679b
Alok Nath Pandey <125994027+aloknathpandey@users.noreply.github.com>
2024-10-04 22:49:33 +0530

    display json data on page

---
+++ src/NewsItem.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+   
+    render() {
+        let {title , description, imageUrl, url} = this.props;
+        return (
+            <div>
+                <div className="card mt-2 mb-2" style={{width:"18rem"}}>
+                    <img src={imageUrl} className="image card-img-top" alt="..."/>
+                        <div className="card-body">
+                            <h5 className="card-title">{title}</h5>
+                            <p className="card-text">{description}...</p>
+                            <a href={url} className="btn btn-sm btn-primary">More</a>
+                        </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default NewsItem
